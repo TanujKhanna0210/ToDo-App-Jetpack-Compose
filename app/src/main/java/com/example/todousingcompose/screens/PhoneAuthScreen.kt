0b87073040bfa9df3b7500b610af880e9de440f0
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-23 19:24:12 +0530

    Composables for entering otp

--- app/src/main/java/com/example/todousingcompose/screens/PhoneAuthScreen.kt
+++ app/src/main/java/com/example/todousingcompose/screens/PhoneAuthScreen.kt
@@ -1,6 +1,7 @@
 package com.example.todousingcompose.screens
 
 import android.app.Activity
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,24 +11,32 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -46,6 +55,12 @@ fun PhoneAuthScreen(activity: Activity) {
         PhoneNumberTextField()
         Spacer(modifier = Modifier.height(16.dp))
         SendOtpButton()
+
+        Spacer(modifier = Modifier.height(80.dp))
+
+        OtpTextField()
+        Spacer(modifier = Modifier.height(16.dp))
+        VerifyButton()
     }
 
 }
@@ -97,6 +112,78 @@ fun SendOtpButton() {
             Icon(imageVector = Icons.Default.Send, contentDescription = null)
             Spacer(modifier = Modifier.width(8.dp))
             Text("Send OTP")
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun OtpTextField() {
+
+    var otpValue by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    BasicTextField(
+        value = otpValue,
+        onValueChange = {
+            if (it.length <= 6) {
+                otpValue = it
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.NumberPassword,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+            }
+        ),
+        decorationBox = {
+            Row(horizontalArrangement = Arrangement.Center) {
+                repeat(6) { index ->
+                    val char = when {
+                        index >= otpValue.length -> ""
+                        else -> otpValue[index].toString()
+                    }
+                    Text(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .wrapContentHeight()
+                            .border(
+                                1.dp,
+                                Color.Gray,
+                                RoundedCornerShape(8.dp)
+                            )
+                            .padding(2.dp),
+                        text = char,
+                        style = MaterialTheme.typography.headlineMedium,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun VerifyButton() {
+    Button(
+        onClick = {
+            // TODO
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = Icons.Default.Done, contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Verify")
         }
     }
 }
